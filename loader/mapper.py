f595d82b6948c1c72ab279886780467b92b9a73b
John Weis <weis.john@gmail.com>
2011-09-07 03:24:18 -0400

    created python traversing logic to build the list of files

--- loader/mapper.py
+++ loader/mapper.py
@@ -12,7 +12,20 @@
             '1' : {
                 'sections' : {
                     '4' : {
-                        1
+                        'files' : { 
+                            '0' : '1',
+                            '1' : '2',
+                        }
+                    }
+                } 
+            },
+            '2' : {
+                'sections' : {
+                    '3' : {
+                        'files' : { 
+                            '0' : '2',
+                            '1' : '4',
+                        }
                     }
                 } 
             }
@@ -20,3 +33,33 @@
         
     });
 '''
+
+import os 
+
+# this traverses the file structure to see if those files are there...
+# finds only the files that in the proper place 
+dirname = '..\\code\\'
+for chapter in os.listdir(dirname):    
+    for section in os.listdir(os.path.join(dirname, chapter)):
+            for file in os.listdir(os.path.join(dirname,chapter,section)):
+                 if os.path.isfile(os.path.join(dirname, chapter, section, file)):
+                     a = 5
+                     #print os.path.join(dirname, chapter, section, file) 
+
+
+
+
+for chapter in os.listdir(dirname):
+    for section in os.listdir(os.path.join(dirname, chapter)):
+            for file in os.listdir(os.path.join(dirname,chapter,section)):
+                 if os.path.isfile(os.path.join(dirname, chapter, section, file)):
+                     print os.path.join(dirname, chapter, section, file) 
+
+       
+       
+       
+       
+       
+       
+       
+       
