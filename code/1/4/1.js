aa134333b2271cdb22d420a7f3b911fa2b713f7e
John Weis <weis.john@gmail.com>
2011-09-07 03:44:39 -0400

    overcoming local SOP, rocking some demo code, da bomb

--- code/1/4/1.js
+++ code/1/4/1.js
@@ -1,4 +1,28 @@
+/*
+ * Best practice: testing
+ * 
+ * a function with a signature like this was mentioned:
+ * 		assert(condition,message);
+ * 
+ * calling something like this:
+ * 		assert(a == 1, "Disaster! A is not 1!");
+ * 
+ * I decided to make a function now, and compare once he gives me his...
+ */
+console.log('running inside 1.js');
 
-(function() { 
-	console.log('do stuff from 1');
-});
+(function() {
+
+	function assert(condition, message) {
+		
+		// modifying a truthy / falsey value and 
+		if (!!!condition) {
+			console.warn(message); 
+		} 
+		
+	}
+	
+	assert(true, "success");	// should show nothing
+	assert(false, "failure"); 	// should consle warn "failure"
+	
+})();
