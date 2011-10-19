6bc051e9259e2a823536ab9fe074e61fdd4adbdd
John Weis <weis.john@gmail.com>
2011-10-19 02:33:38 -0400

    long-running locking and long-running worker examples

---
+++ code/6/3/1.js
@@ -0,0 +1,23 @@
+/*
+ * Concept: 
+ * 
+ * locking a browser is a stupid thing to do
+ * 
+ * Chrome / FF7.0 hate this code, locking on purpose
+ * 
+ */
+
+(function() {
+
+	var start = (new Date).getTime(),
+		stop = (new Date).getTime(),
+		seconds = 10;
+		
+	// don't let the browser have control for 10 secs
+	while (stop - start < (1000 * 10)) {
+		stop = (new Date).getTime();
+	}
+	console.log('finished');
+
+})();
+
---
+++ code/6/3/2.js
@@ -0,0 +1,41 @@
+/*
+ * Concept: breaking up a task into more usable chunks
+ * 
+ * because we use a setTimeout, this allows us to 
+ * compute our task (which is just a counter in 
+ * this example), we release control back to the 
+ * browser so that we don't freeze the animation frames 
+ * 
+ */
+
+(function() {
+
+	// build up a large amount of work
+	var start, stop,
+		seconds = 20,	// a large amount of work
+		steps = 20 * seconds, // break the work down
+		iteration = 0; 	// keep track of current work
+	
+	// worker
+	setTimeout(function() { 
+		console.log('iteration:', iteration++);
+	
+		start = (new Date).getTime();
+		stop = (new Date).getTime();
+	
+		// do some work for a fraction of the time
+		while (stop-start < ((1000 * seconds) / steps)) {
+			// console.log('new time');
+			stop = (new Date).getTime();
+		}
+		
+		if (iteration < steps) {
+			// this releases control back to 
+			// browser animation frame
+			setTimeout(arguments.callee, 0);
+		}
+	
+	},5000); // invoke 5 seconds after registering
+	
+
+})();
