66ad55dca0ab6909b6a1f2a9c5b588aae316d8fe
John Weis <weis.john@gmail.com>
2012-10-16 12:45:30 -0400

    just wrote a sample URL dispatcher based on capturing

--- code/7/4/1.js
+++ code/7/4/1.js
@@ -1,21 +1,48 @@
 /*
- * Concept: Generic Compilation
+ * Concept: Basic Captures
  * 
- * 
+ * A sample URL router, i.e. a sample purpose of using the result of a capture as a dispatcher.
  * 
  */
 
 (function() {
 
-	function find_in_string(match, source) {
+	// a sample app-like structure to handle routes
+	var routes = {
+		
+		"app" : function() { 
+			console.log('looking for the app'); 
+		},
 
-		// double escape 
-		var regex = new RegExp("(^|\\s)" + match + "(\\s|$)");
-
-		console.log( regex.test(source) );
+		"bar" : function(id) { 
+			if (id) {
+				console.log('looking for a bar with an id of ' + id);
+			} else {
+				console.log('looking for all bars');
+			}
+		}
 	}
 
-	find_in_string("sample", "A sample test string");
-	find_in_string("false", "This isn't in the string");
+	var url_processor = /\/(\w+)(\/(\w+))?/;
+
+	function dispatch_path(relative_url) {
+
+		var route_parts = relative_url.match(url_processor);
+		var route = route_parts[1];
+
+		// if we have something registered for this route, dispatch it
+		if (typeof routes[route] == "function") {
+			var args = null;
+			if (route_parts[2] != undefined) {
+				args = route_parts[3];
+			}
+			routes[route]( args );
+		}
+
+	}
+
+	dispatch_path("/app");
+	dispatch_path("/bar");
+	dispatch_path("/bar/1");
 
 })();
