79005d5685cc895095334c7a3c2f1062d0826c7e
John Weis <weis.john@gmail.com>
2012-10-21 20:02:44 -0400

    cleanup

--- code/4/4/1.js
+++ code/4/4/1.js
@@ -7,13 +7,13 @@
 (function(){
 
 
-    Function.prototype.memoized = function(key){
+    Function.prototype.memoized = function(key) {
         this._values = this._values || {};
         return this._values[key] !== undefined ? this._values[key] : this._values[key] = this.apply(this, arguments);
     };
 	
 	
-    function isPrime(num){
+    function isPrime(num) {
         var prime = num != 1;
         for (var i = 2; i < num; i++) {
             if (num % i == 0) {
