f27e3058efc4ba43f324195638429369e618cf10
John Weis <weis.john@gmail.com>
2012-10-22 00:53:43 -0400

    learning how to :

---
+++ code/9/1/1.js
@@ -0,0 +1,56 @@
+/*
+ * Concept: Run-Time Code Evaluation
+ * 
+ * two aspects of eval():
+ * 	- it will evaluate the code passed to it as a string
+ *  - it will execute that code in the scope in which eval() is called
+ */
+
+(function() {
+
+	// any `code` passed in is `eval()`ed
+	// if `assertion` is a function, invoke and test 
+	// if `assertion` is a string, it will be `eval()`ed as well and tested
+	// if `assertion` is a value, the result of the `code` that was `eval()`ed will be compared
+	function eval_and_assert(code, assertion) {
+
+		// this is a bit terse, ret is used to see what `eval()` returns 
+		
+
+		// try to eval the code, catch errors
+		try {
+			// what was *returned* from our `eval()`
+			var returned = code !== undefined ? eval(code) : eval();	
+		} catch (e) {
+			console.error(e);
+		}
+		
+		// the result of our assertion test
+		var result;
+		if (typeof assertion === "function") {
+			result = assertion();
+		} else if (typeof assertion === "string") {
+			result = eval(assertion);
+		} else {
+			result = (returned === assertion);
+		}
+
+		console.log(
+			'eval(' + code + ') ==', assertion , 
+			"\n" + returned , ":",  (!!result ? "success" : "failure") );
+
+		return returned;
+	}
+
+	// eval returns values
+	eval_and_assert("2 + 2", 5);
+
+	// eval creates functions
+	eval_and_assert("function foo() { return '42'; }", "(function() { return foo() == 42; })()");
+
+	// eval creates objects
+	eval_and_assert("var a = { 'b' : 2 };", "(function() { return typeof a !== \"undefined\" })()");
+
+
+})();
+
