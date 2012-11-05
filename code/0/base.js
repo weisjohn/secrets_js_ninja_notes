cdf2c51d532a487ae0338ec7cc4eb6437c93f363
John Weis <weis.john@gmail.com>
2012-11-05 15:56:06 -0500

    expanding the benching function to accept a name

--- code/0/base.js
+++ code/0/base.js
@@ -1,18 +1,23 @@
 
 // simple timer for performance calculations
-function bench(iterations, test) {
+function bench(iterations, test, name) {
+
+	if (!!name) {
+		console.log('benching: ' + name);
+	}
 
 	// internal IIFE to create a separate scope
 	
-		if (typeof test == "function") {
-			var start = Date.now();
-			console.log('start: ' + start);
-			for (var i = 0; i < iterations; i++) {
-				test();
-			}
-			var end = Date.now();
-			console.log('end: ' + end);
-			console.log(iterations + ' iterations took ' + (end-start) + 'ms');
+	if (typeof test == "function") {
+		var start = Date.now();
+		console.log('start: ' + start);
+		for (var i = 0; i < iterations; i++) {
+			test();
 		}
-	
+		var end = Date.now();
+		console.log('end: ' + end);
+		console.log(iterations + ' iterations took ' + (end-start) + 'ms');
+	}
+
+		
 }
