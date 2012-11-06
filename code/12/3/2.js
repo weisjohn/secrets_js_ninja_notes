3855d9a08e4a27a45cc16eb0a2afae6ed0a701d5
John Weis <weis.john@gmail.com>
2012-11-06 13:55:13 -0500

    RGB to HSL... sort of...

--- code/12/3/2.js
+++ code/12/3/2.js
@@ -1,49 +1,81 @@
 /*
- * Concept: HSL to RGB
+ * Concept: HSL to RGB conversion
  * 
  */
 
 (function() {
 
-	window.getColor = function(element, property) {
-		// name = translations[name] || name;
+	var rgb_regex = /rgb\((.+)\)/;
 
-	    name = name.replace(/-([a-z])/ig, function (all, letter) {
-	        return letter.toUpperCase();
-	    });
-	    if (typeof value !== 'undefined') {
+	window.getHSLColor = function(element, property) {
+		
+		if (!element) return;	// leave quickly
 
-	    	// isNaN tests if the string has any characters that aren't digits...
-	    	if (!isNaN(value)){
-	    		if (!non_pixel_value_types[name]) {
-	    			value = parseInt(value) + "px";
-	    		}
-	    	}
+		var color = element.style[property],
+			rgb = [],
+			hsl;
 
-	        element.style[name] = value;
-	    }
-	    return element.style[name];
+		// detect if the color is of the form `rbg(x,y,z)`
+		if (rgb_regex.test(color)) {
+
+			// make into rgb array
+			color.match(rgb_regex)[1].split(',').forEach(function(elem, i) { 
+				rgb[i] = parseInt(elem);
+			}); 
+
+			console.dir(rgb);
+
+			// make the formulas easy to read
+			var red 	= rgb[0], 
+				green 	= rgb[1], 
+				blue 	= rgb[2];
+
+			// convert it to HSL 
+			// http://en.wikipedia.org/wiki/HSL_and_HSV#General_approach
+
+			// chroma is the difference of the Max(rgb) - Min(rgb)
+			var max = Math.max.apply(null, rgb);
+			var min = Math.min.apply(null, rgb);
+
+			var hue, saturation, lightness = (max + min) / 2;
+
+			if (max == min) {
+				hue = saturation = 0;
+			} else {
+				var delta = max - min;
+				saturation = (lightness > 0.5) ? delta / (2 - max - min) : d / (max + min);
+
+				switch (max) {
+					case red : 
+						hue = (green - blue) / d + ( green < blue ? 6 : 0 ); 
+						break;
+					case green :
+						hue = (blue - red) / d + 2;
+						break;
+					case blue : 
+						hue = (red - green) / d + 4; 
+						break;
+				}
+				hue = hue / 6;
+			}
+
+			return {
+				"hue" : hue,
+				"saturation" : saturation,
+				"lightness" : lightness
+			};
+
+		}
+
 	}
 
-	// main header in the document
+	// main header in the document, has a hex #333333 for color
 	var h1 = document.getElementsByTagName('h1')[0];
+	var h1_hsl = getHSLColor(h1, 'color');
 
-	assert(style(h1, 'margin-top') == "40px", 'h1 has a 40px margin at the top');
+	assert(h1_hsl['hue'] == '0', 'hue is 0°s');
+	assert(h1_hsl['saturation'] == '0', 'saturation is 0');
+	assert(h1_hsl['lightness'] == '51', 'lightness is 0');
 
-
-	var test_element = document.createElement('div');
-	test_element.appendChild( document.createTextNode('make money on the float') );
-	document.body.insertBefore(test_element);
-
-	style(test_element, 'float', 'left');
-	assert(style(test_element, 'float') == "left", 'test div has float:left;');
-
-	assert(style(test_element, 'font-size', '12') == "12px", 'test div font-size:12px;');
-	
-	assert(style(test_element, 'font-size', '12em') == "12em", 'test div font-size:12em;');
-
-	assert(style(test_element, 'z-index', '100') == "100", 'test div z-index:100;');
-
-	document.body.removeChild(test_element);
 
 })();
